bd007d45fd3ab976d5e63f76ae0330b079740115
spookyorange <korkmazcoltu@mail.ru>
2022-03-10 15:35:21 +0300

    profile and post models created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_10_120536) do
+ActiveRecord::Schema.define(version: 2022_03_10_123437) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "profile_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["profile_id"], name: "index_posts_on_profile_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "username"
+    t.integer "age"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +42,6 @@ ActiveRecord::Schema.define(version: 2022_03_10_120536) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "profiles"
+  add_foreign_key "profiles", "users"
 end
