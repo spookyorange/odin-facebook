1c69e5d5a8b07bc9b82c7e3b229d7ca121b83c7b
spookyorange <korkmazcoltu@mail.ru>
2022-03-11 16:36:16 +0300

    now posts can be deleted, not profiles

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
 
-  resources :posts, except: :destroy do
+  resources :posts do
     resources :comments
   end
 
-  resources :profiles
+  resources :profiles, except: :destroy
   resources :likes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
