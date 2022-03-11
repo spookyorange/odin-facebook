075036147f9bdf16e01624a4f6642c075bd004e5
spookyorange <korkmazcoltu@mail.ru>
2022-03-11 15:58:13 +0300

    route for profile and post changed for better experience

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
 
-  resources :profiles, except: :destroy do
+  resources :posts, except: :destroy do
     resources :comments
   end
 
-  resources :posts
+  resources :profiles
   resources :likes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
