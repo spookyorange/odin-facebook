2bf8bd5c29cc64912417384d3e44fb37da9284f5
spookyorange <korkmazcoltu@mail.ru>
2022-03-12 14:00:37 +0300

    routes altered for comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :posts do
-    resources :comments
+    resources :comments, except: [:index, :show]
   end
 
   resources :profiles, except: :destroy
