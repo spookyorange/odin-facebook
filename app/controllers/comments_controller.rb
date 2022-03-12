1f0c9379228cee85a01b206fa34803109d5731e4
spookyorange <korkmazcoltu@mail.ru>
2022-03-12 15:34:47 +0300

    comments controllers and view generated, working on some issues, quicksaving

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,47 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.profile = current_user.profile
+    @comment.save
+    redirect_to @post
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+
+    if @comment.profile != current_user.profile
+      flash[:alert] = 'you can not edit a comment that is not yours'
+      redirect_to @post
+    else
+      if @comment.update(comment_params)
+        flash[:notice] = 'comment edited successfully'
+        redirect_to @post
+      else
+        flash[:notice] = 'something went wrong'
+        redirect_to @post
+      end
+    end
+
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+
+    if @comment.profile != current_user.profile
+      flash[:alert] = 'you can not delete a comment that is not yours'
+      redirect_to @post
+    else
+      @comment.destroy
+      redirect_to @post
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
---
+++ db/migrate/20220312123319_add_username_to_comments_table.rb
@@ -0,0 +1,5 @@
+class AddUsernameToCommentsTable < ActiveRecord::Migration[6.1]
+  def change
+    
+  end
+end
