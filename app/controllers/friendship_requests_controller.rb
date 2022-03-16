19a1e2ad32db6fb1283646eca29902d7c4c58176
spookyorange <korkmazcoltu@mail.ru>
2022-03-16 14:08:48 +0300

    Friendship Logic implemented to controllers / Views

--- app/controllers/friendship_requests_controller.rb
+++ app/controllers/friendship_requests_controller.rb
@@ -7,38 +7,48 @@ class FriendshipRequestsController < ApplicationController
   def create
     @freq = FriendshipRequest.new(friend_param)
     @freq[:inviter_id] = current_user.profile.id
+    @excluded = []
+    @excluded << current_user.profile.friends
+    @excluded << current_user.profile
+    @existing = FriendshipRequest.where("invitee_id = ? AND inviter_id = ?", @freq[:invitee_id], @freq[:inviter_id])
+    @existing2 =FriendshipRequest.where("inviter_id = ? AND invitee_id = ?", @freq[:invitee_id], @freq[:inviter_id])
 
-    if @freq.save
+    if @existing.empty? && @existing2.empty? && @excluded.exclude?(@freq.invitee) && @freq.save
       flash[:notice] = 'friendship request sent successfully'
-      render :'profiles/index'
+      redirect_to profiles_path
     else
       flash.now[:notice] = 'something went wrong'
-      render :'profiles/index'
+      redirect_to profiles_path
     end
   end
 
   def destroy
     @freq = FriendshipRequest.find(params[:id])
-    if params[:accept] == 1
-      flash.now[:notice] = 'accepted friend request'
+    @acceptance = params[:friendship_request][:accept].to_i
+    if @acceptance == 1 && current_user.profile == @freq.invitee
+      flash[:notice] = 'accepted friend request'
       @freq.inviter.friends << @freq.invitee
       @freq.invitee.friends << @freq.inviter
       @freq.destroy
-      render :'friendship_requests/index'
-    elsif params[:accept] == 0
-      flash.now[:notice] = 'refused friend request'
+      redirect_to friendship_requests_path
+    elsif @acceptance == 0 && current_user.profile == @freq.invitee
+      flash[:notice] = 'refused friend request'
       @freq.destroy
-      render :'friendship_requests/index'
+      redirect_to profiles_path
+    elsif @acceptance == 0 && current_user.profile == @freq.inviter
+      flash[:notice] = 'cancelled friend request'
+      @freq.destroy
+      redirect_to friendship_requests_path
     else
-      flash.now[:notice] = 'something went wrong'
-      render :'friendship_requests/index'
+      flash[:notice] = 'something went wrong'
+      redirect_to friendship_requests_path
     end
   end
 
   private
 
   def friend_param
-    params.require(:FriendshipRequest).permit(:invitee_id)
+    params.require(:friendship_request).permit(:invitee_id)
   end
 
 end
